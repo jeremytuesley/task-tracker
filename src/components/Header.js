7683b5d91af7148928bb0f2d558e367ceee9408a
jeremytuesley <jeremytuesley@gmail.com>
2022-04-29 21:27:54 +1000

    file setup and css imported

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// experimenting with props
+
+export default Header;
